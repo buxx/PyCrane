a44d4ff896dd89b9e9683243edb756e753f35c6b
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-11-20 07:04:25 +0100

    add some exemple config

--- sandbox/wordpress/config.py
+++ sandbox/wordpress/config.py
@@ -3,11 +3,19 @@ HOSTS = {
     'HOST_A': {
         'hostname': 'localhost',
         'socket': 'unix://var/run/docker.sock'
+    },
+    'HOST_B': {
+        'hostname': 'localhost',
+        'socket': 'unix://var/run/docker.sock'
     }
 }
 
 APPS = {
     'WORDPRESS': {
+        'image': 'TODO',
+        # TODO: methode de deploiement
+    },
+    'MYSQL': {
         'image': 'TODO',
         # TODO: methode de deploiement
     }
